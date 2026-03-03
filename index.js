de1e20e968c226af8466165cce278e22694e5336
ivolzok <ivolzok@gmail.com>
2026-03-03 17:48:03 +0500

    addParser

--- index.js
+++ index.js
@@ -16,10 +16,28 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            let a = readFile('console.js')
+            console.log(get_TODOes(a));
         default:
             console.log('wrong command');
             break;
     }
+}
+
+function get_TODOes(text){
+    let res = []
+    let strings = text.split('\n');
+    for (let str of strings) {
+        let splited = str.split('//');
+        if (splited.length <= 1)
+            continue;
+        let comment = splited[1].trim();
+        if (comment.startsWith('TODO')){
+            res.push(comment);
+        }
+    }
+    return res;
 }
 
 // TODO you can do it!
