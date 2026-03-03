a289665d167ee280fac8c5408d32e478faf55e4a
ivolzok <ivolzok@gmail.com>
2026-03-03 18:48:22 +0500

    add 4

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    switch (command.split(' ')[0]) {
         case 'exit':
             process.exit(0);
             break;
@@ -20,7 +20,20 @@ function processCommand(command) {
             executeShow()
             break;
         case 'important':
-            getImportntToDos();
+            getImportantToDoes();
+            break;
+        case 'sort':
+            const arg = command.split(' ')[1];
+            let comments = getAllTODOes()
+            if (arg === 'importance'){
+                executeSortImportance(comments)
+            }
+            else if (arg === 'user'){
+                executeSortUser(comments)
+            }
+            else if (arg === 'date'){
+                executeSortDate(comments)
+            }
             break;
         default:
             console.log('wrong command');
@@ -33,6 +46,41 @@ function executeShow(){
     console.log(comments);
 }
 
+function executeSortImportance(comments){
+    comments.sort((a,b) => -(a.split('!').length - b.split('!').length))
+    console.log(comments);
+}
+
+function executeSortUser(comments){
+    comments.sort(function (a, b){
+        const aSplited = a.split(';')
+        if (aSplited.length <= 1){
+            return 1;
+        }
+        const bSplited = b.split(';')
+        const aName = aSplited[0].split(' ').slice(1).join(' ').trim();
+        const bName = bSplited[0].split(' ').slice(1).join(' ').trim();
+        if (aName === 'Anonymous Developer')
+            return 1;
+        return bName.localeCompare(aName);
+    })
+    console.log(comments);
+}
+
+function executeSortDate(comments){
+    comments.sort(function (a, b){
+        const aSplited = a.split(';')
+        if (aSplited.length <= 1){
+            return 1;
+        }
+        const bSplited = b.split(';')
+        const aDate = new Date(aSplited[1].trim())
+        const bDate = new Date(bSplited[1].trim())
+        return bDate.getTime() - aDate.getTime();
+    })
+    console.log(comments);
+}
+
 function getAllTODOes() {
     let comments = []
     let files = getFiles()
@@ -41,7 +89,7 @@ function getAllTODOes() {
     }
     return comments;
 }
-function getImportntToDos(){
+function getImportantToDoes(){
     for (let toDo of getAllTODOes()) {
         if (toDo.includes('!')) {
             console.log(toDo);
